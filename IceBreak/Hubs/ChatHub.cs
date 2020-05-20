478ce15c0367c26b3431154aca8dd1adaf2b3ae4
ina <superpucy@gmail.com>
2020-05-20 17:03:34 +0800

    加入專案檔案。

---
+++ IceBreak/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace IceBreak.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendSort(string user, string[] list)
+        {
+            await Clients.All.SendAsync("ReceiveSort", user, list);
+        }
+    }
+}
